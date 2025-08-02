1258acb6e4b61f0fd690b1a9e0f75c5789fb3ba5
Faure <fbleza5@gmail.com>
2025-08-02 08:33:44 +0000

    pixel

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import ProblemSection from './components/ProblemSection';
-import SolutionSection from './components/SolutionSection';
-import ContentPreview from './components/ContentPreview';
-import Testimonials from './components/Testimonials';
-import AuthorSection from './components/AuthorSection';
-import OfferSection from './components/OfferSection';
-import FAQ from './components/FAQ';
-import FinalCTA from './components/FinalCTA';
-import Footer from './components/Footer';
-import MentionsLegales from './pages/MentionsLegales';
-import PolitiqueConfidentialite from './pages/PolitiqueConfidentialite';
-import CGV from './pages/CGV';
-import SuccessPage from './pages/SuccessPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import ProblemSection from "./components/ProblemSection";
+import SolutionSection from "./components/SolutionSection";
+import ContentPreview from "./components/ContentPreview";
+import Testimonials from "./components/Testimonials";
+import AuthorSection from "./components/AuthorSection";
+import OfferSection from "./components/OfferSection";
+import FAQ from "./components/FAQ";
+import FinalCTA from "./components/FinalCTA";
+import Footer from "./components/Footer";
+import MentionsLegales from "./pages/MentionsLegales";
+import PolitiqueConfidentialite from "./pages/PolitiqueConfidentialite";
+import CGV from "./pages/CGV";
+import SuccessPage from "./pages/SuccessPage";
+import { useEffect } from "react";
 
 const HomePage = () => (
   <div className="min-h-screen bg-white">
@@ -31,13 +32,36 @@ const HomePage = () => (
   </div>
 );
 
+const TikTokPixel = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+
+  ttq.load('D26SJ1JC77U916NIVUP0');
+  ttq.page();
+}(window, document, 'ttq');
+ `;
+    document.body.appendChild(script);
+  }, []);
+  return null;
+};
+
 function App() {
   return (
     <Router>
+      <TikTokPixel />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
-        <Route path="/politique-confidentialite" element={<PolitiqueConfidentialite />} />
+        <Route
+          path="/politique-confidentialite"
+          element={<PolitiqueConfidentialite />}
+        />
         <Route path="/cgv" element={<CGV />} />
         <Route path="/success" element={<SuccessPage />} />
       </Routes>
